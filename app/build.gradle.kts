a1c332fb7201ba51107c07f459814254d90a01fb
jiaxinxs <eileenyan1994@gmail.com>
2024-03-30 13:34:08 -0400

    finished Javadoc and tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-//    implementation(fileTree(mapOf("dir" to "C:\\Users\\eilee\\AppData\\Local\\Android\\Sdk\\platforms\\android-34", "include" to listOf("*.aar", "*.jar"), "exclude" to listOf())))
+  //  implementation(fileTree(mapOf("dir" to "C:\\Users\\eilee\\AppData\\Local\\Android\\Sdk\\platforms\\android-34", "include" to listOf("*.aar", "*.jar"), "exclude" to listOf())))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
